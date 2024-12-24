73a693ec9ac214a18e859adab116400f3ac7ee10
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2024-12-24 13:04:22 +0100

    add some more tests

--- integration_test.go
+++ integration_test.go
@@ -3,9 +3,104 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
 	"testing"
 )
+
+func TestWrongPhoneNumberFails(t *testing.T) {
+	server := createAndStartTestServer(t)
+	defer server.Stop()
+
+    phone := "+31612345678"
+
+    resp, err := makeVerifyRequest(phone, "123456")
+    if err != nil {
+        t.Fatalf("failed to send verify request")
+    }
+
+    if resp.StatusCode != http.StatusBadRequest {
+		t.Fatalf(
+			"status code was expected to be %v (BadRequest), but was %v",
+			http.StatusBadRequest,
+			resp.StatusCode,
+		)
+    }
+}
+
+func TestWrongTokenFails(t *testing.T) {
+	server := createAndStartTestServer(t)
+	defer server.Stop()
+
+	phone := "+31612345678"
+
+	resp, err := makeSendSmsRequest(phone, "en")
+
+	if err != nil {
+		t.Fatalf("send sms request failed: %v", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("send-sms response status code not ok: %v", resp.StatusCode)
+	}
+
+	resp, err = makeVerifyRequest(phone, "111111")
+
+	if err != nil {
+		t.Fatalf("verify request failed: %v", err)
+	}
+
+	if resp.StatusCode != http.StatusUnauthorized {
+		t.Fatalf(
+			"status code was expected to be %v (unauthorized), but was %v",
+			http.StatusUnauthorized,
+			resp.StatusCode,
+		)
+	}
+}
+
+func TestSendingSendRequest(t *testing.T) {
+	server := createAndStartTestServer(t)
+	defer server.Stop()
+
+	phone := "+31612345678"
+
+	resp, err := makeSendSmsRequest(phone, "en")
+
+	if err != nil {
+		t.Fatalf("send sms request failed: %v", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("send-sms response status code not ok: %v", resp.StatusCode)
+	}
+
+	resp, err = makeVerifyRequest(phone, "123456")
+
+	if err != nil {
+		t.Fatalf("verify request failed: %v", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("verify response status code not OK: %v", resp.StatusCode)
+	}
+
+	body, err := readCompleteBodyToString(resp)
+	if body != "JWT" {
+		t.Fatalf("body not JWT: %v, %v", err, body)
+	}
+}
+
+// ------------------------------------------------------------------------
+
+func readCompleteBodyToString(r *http.Response) (string, error) {
+	defer r.Body.Close()
+	bytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
 
 type mockSmsSender struct {
 	sent map[string]string
@@ -54,8 +149,9 @@ func createAndStartTestServer(t *testing.T) *Server {
 
 	go func() {
 		err := server.ListenAndServe()
-		if err != nil {
-			ErrorLogger.Printf("server crashed: %v", err)
+		// if server didn't close succesfully
+		if err != http.ErrServerClosed {
+			ErrorLogger.Fatalf("server crashed: %v", err)
 		}
 	}()
 	return server
@@ -73,19 +169,14 @@ func makeSendSmsRequest(phone, language string) (*http.Response, error) {
 	return http.Post("http://127.0.0.1:8081/send", "application/json", bytes.NewBuffer(json))
 }
 
-func TestSendingSendRequest(t *testing.T) {
-	server := createAndStartTestServer(t)
-	defer server.Stop()
-
-	resp, err := makeSendSmsRequest("+31612345678", "en")
-
+func makeVerifyRequest(phone, token string) (*http.Response, error) {
+	payload := VerifyPayload{
+		PhoneNumber: phone,
+		Token:       token,
+	}
+	json, err := json.Marshal(payload)
 	if err != nil {
-		t.Fatalf("making a response failed: %v", err)
+		return nil, err
 	}
-
-	if resp.StatusCode != http.StatusOK {
-		t.Fatalf("status code not ok: %v", resp.StatusCode)
-	} else {
-		t.Log("Test completed successfully")
-	}
+	return http.Post("http://127.0.0.1:8081/verify", "application/json", bytes.NewBuffer(json))
 }
