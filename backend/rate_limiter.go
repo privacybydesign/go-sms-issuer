8cdbe2656258d8c7be027e8b16b5e2a974512a46
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2024-12-30 13:38:23 +0100

    improvements to ratelimiting

--- backend/rate_limiter.go
+++ backend/rate_limiter.go
@@ -18,9 +18,9 @@ type DefaultRateLimiter struct {
 }
 
 type ClientLimiter struct {
-	tries            int
-	timeoutDuration  time.Duration
-	lastRequest      time.Time
+	tries           int
+	timeoutDuration time.Duration
+	lastRequest     time.Time
 }
 
 func NewDefaultRateLimiter(clock Clock) *DefaultRateLimiter {
@@ -31,62 +31,51 @@ func NewDefaultRateLimiter(clock Clock) *DefaultRateLimiter {
 }
 
 func getTimeout(tries int) time.Duration {
-    if tries < 3 {
-        return 0
-    }
-    if tries < 4 {
-        return time.Minute
-    }
-    if tries < 5 {
-        return 5 * time.Minute
-    }
-    if tries < 6 {
-        return time.Hour
-    }
-    return 24 * time.Hour
+	if tries < 3 {
+		return 0
+	}
+	if tries < 4 {
+		return time.Minute
+	}
+	if tries < 5 {
+		return 5 * time.Minute
+	}
+	if tries < 6 {
+		return time.Hour
+	}
+	return 24 * time.Hour
 }
 
 func (r *DefaultRateLimiter) Allow(ip, phone string) (allow bool, timeout time.Duration) {
-	client := r.getClientLimiter(ip, phone)
-	now := time.Now()
-
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	// if the number of requests is less than 3, let it pass
-	if client.tries < 3 {
-		client.tries += 1
-		client.lastRequest = now
-		return true, 0
-	}
+	client := r.getClientLimiter(ip, phone)
+	now := r.clock.GetTime()
 
-	// tries is over 3
+	// tries is 3 or higher
 	timeSinceLastRequest := now.Sub(client.lastRequest)
 	remaining := client.timeoutDuration - timeSinceLastRequest
 
 	if remaining > 0 {
 		// the timeout is not over yet, don't increment the number of attempt,
-        // but also don't allow this one to pass and nofity about the remaining timeout
+		// but also don't allow this one to pass and nofity about the remaining timeout
 		return false, remaining
 	} else {
 		// timeout is over, pass the request, but set a new timeout
-        client.tries += 1
-        client.lastRequest = now
-        client.timeoutDuration = getTimeout(client.tries)
-        return true, 0
+		client.tries += 1
+		client.lastRequest = now
+		client.timeoutDuration = getTimeout(client.tries)
+		return true, 0
 	}
 }
 
 func (r *DefaultRateLimiter) getClientLimiter(ip, phone string) *ClientLimiter {
 	key := fmt.Sprintf("%v&%v", ip, phone)
 
-	r.mu.Lock()
-	defer r.mu.Unlock()
-
 	if _, exists := r.limits[key]; !exists {
 		r.limits[key] = &ClientLimiter{
-			tries:            0,
-			timeoutDuration:  1 * time.Minute,
+			tries: 0,
 		}
 	}
 	return r.limits[key]
--- backend/rate_limiter_test.go
+++ backend/rate_limiter_test.go
@@ -13,7 +13,12 @@ func (c *mockClock) GetTime() time.Time {
 	return c.time
 }
 
-func (c *mockClock) IncTime(time.Duration) {
+func (c *mockClock) IncTime(time time.Duration) {
+	c.time = c.time.Add(time)
+}
+
+func between(min, max, value time.Duration) bool {
+	return value >= min && value <= max
 }
 
 func TestRateLimiter(t *testing.T) {
@@ -31,13 +36,48 @@ func TestRateLimiter(t *testing.T) {
 			t.Fatalf("request %v should not be rate limited: %v", i, timeRemaining)
 		}
 
-        clock.IncTime(time.Second)
+		clock.IncTime(time.Second)
 	}
 
 	// fourth request should be rate limited
-	allow, _ := rl.Allow(ip, phone)
+	allow, timeRemaining := rl.Allow(ip, phone)
 
 	if allow {
 		t.Fatalf("expected fourth request to be limited")
+	}
+
+	if timeRemaining.Round(time.Minute) != time.Minute {
+		t.Fatalf("remaining time should be about a minute, but was %v", timeRemaining)
+	}
+
+	// half a minute later the request should still be rejected, but time remaining should have decreased
+	clock.IncTime(30 * time.Second)
+	allow, timeRemaining = rl.Allow(ip, phone)
+
+	if allow {
+		t.Fatalf("expected fourth request remain limited after 30 seconds")
+	}
+
+	if !between(25*time.Second, 30*time.Second, timeRemaining) {
+		t.Fatalf("expected time remaining to be about 30 seconds, but was %v", timeRemaining)
+	}
+
+	// another half a minute later the timeout should be over
+	clock.IncTime(30 * time.Second)
+	allow, timeRemaining = rl.Allow(ip, phone)
+
+	if !allow {
+		t.Fatalf("expected timeout to be over after a minute: time remaining %v", timeRemaining)
+	}
+
+	// next request should then have a new timeout of 5 minutes
+	clock.IncTime(30 * time.Second)
+	allow, timeRemaining = rl.Allow(ip, phone)
+
+	if allow {
+		t.Fatalf("expected timeout after 5th request")
+	}
+	if timeRemaining.Round(time.Minute) != 5*time.Minute {
+		t.Fatalf("expected 5 minute timeout")
 	}
 }
