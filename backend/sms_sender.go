05b0571dac48362dcc3a7300e2154141de14fe48
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2025-01-02 17:00:23 +0100

    update cm sms sender code

--- backend/sms_sender.go
+++ backend/sms_sender.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"bytes"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"strings"
+	"time"
 )
 
 type SmsSender interface {
@@ -15,33 +19,70 @@ type CmSmsSenderConfig struct {
 	ApiEndpoint  string `json:"api_endpoint"`
 	ProductToken string `json:"product_token"`
 	Reference    string `json:"reference"`
+	TimeoutMs    int64  `json:"timeout_ms"`
 }
 
 type CmSmsSender struct {
 	CmSmsSenderConfig
 }
 
+func NewCmSmsSender(config CmSmsSenderConfig) (*CmSmsSender, error) {
+	if strings.HasPrefix(config.ApiEndpoint, "https://") {
+		return nil, errors.New("CM gateway API endpoint should use https")
+	}
+
+	if strings.HasSuffix(config.ApiEndpoint, "/") {
+		return nil, errors.New("CM endpoint should not end with /")
+	}
+
+	return &CmSmsSender{config}, nil
+}
+
 func (s *CmSmsSender) SendSms(phone, message string) error {
-	reqContent := url.Values{}
-	reqContent.Add("phone", phone)
-	reqContent.Add("message", message)
+	if phone[0] == '+' {
+		phone = fmt.Sprintf("00%s", phone[1:])
+	} else if phone[:2] != "00" {
+		return errors.New("CM expects internationalized phone numbers")
+	}
 
-	// url encode the values
-	reqBody := reqContent.Encode()
-	contentType := "application/x-www-form-urlencoded; charset=UTF-8"
-	_, err := http.Post(s.ApiEndpoint, contentType, bytes.NewBuffer([]byte(reqBody)))
+	params := url.Values{}
+	params.Add("producttoken", s.ProductToken)
+	params.Add("from", s.From)
+	params.Add("to", phone)
+	params.Add("body", message)
+	params.Add("reference", s.Reference)
 
+	fullUrl := fmt.Sprintf("%s/gateway.ashx?%s", s.ApiEndpoint, params.Encode())
+
+	client := &http.Client{Timeout: time.Duration(s.TimeoutMs) * time.Millisecond}
+
+	resp, err := client.Get(fullUrl)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to send request: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("CM gateway returned status code %d", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read response: %w", err)
+	}
+
+	text := string(body)
+	if text != "" {
+		return fmt.Errorf("error response received from: %s", text)
 	}
 
 	return nil
 }
 
-type DummySmsSender struct {}
+type DummySmsSender struct{}
 
 func (s *DummySmsSender) SendSms(phone, message string) error {
-    InfoLogger.Printf("Sending sms to %v: %v", phone, message)
-    return nil
+	InfoLogger.Printf("Sending sms to %v: %v", phone, message)
+	return nil
 }
-
