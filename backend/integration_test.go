cfa12fc4b2e4f035a839f56eaf5189deed399ea7
Dibran Mulder <dibran.mulder@outlook.com>
2025-07-08 10:46:26 +0200

    Added tests

--- backend/integration_test.go
+++ backend/integration_test.go
@@ -67,6 +67,7 @@ func TestRateLimitingSingleClient(t *testing.T) {
 		}
 	}
 
+	// third request should be getting rate limited
 	resp, err := makeSendSmsRequest(phone, "en", testCaptha)
 	if err != nil {
 		t.Fatalf("failed to send sms request: %v", err)
@@ -277,6 +278,7 @@ func (m *mockJwtCreator) CreateJwt(phone string) (string, error) {
 func createAndStartTestServer(t *testing.T, smsChan *chan smsMessage, turnstileSuccess bool) *Server {
 	smsSender := newMockSmsSender(smsChan)
 	turnstileVerifier := NewMockTurnStileVerifier(turnstileSuccess)
+
 	ipRateLimitingPolicy := rate.RateLimitingPolicy{
 		Window: time.Minute * 30,
 		Limit:  10,
