ff320a7a32602f58018ed8c89a0e4031b2099f83
saravahdatipour <saravahdatipour@gmail.com>
2025-07-23 11:25:32 +0200

    add support for opening sms link

--- frontend/src/pages/Enroll.tsx
+++ frontend/src/pages/Enroll.tsx
@@ -17,10 +17,69 @@ export default function EnrollPage() {
   const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
   const { phoneNumber, setPhoneNumber } = useAppContext();
 
+
   useEffect(() => {
-    setMessage(t('sms_sent'));
+    setMessage(t("sms_sent"));
   }, [phoneNumber]);
 
+
+  // whether the user types the verification code or with the deeplink in the SMS we verify and start the issuance
+  const VerifyAndStartIssuance = async (phoneNumber: string, token: string) => {
+  const parsedPhoneNumber = parsePhoneNumberFromString(phoneNumber);
+
+  try {
+  // send phone number and token to verify endpoint to see if the token is valid for this phone number
+  const response = await fetch("/verify", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      phone: parsedPhoneNumber?.number,
+      token: token,
+    }),
+  });
+
+  if (response.ok) {
+    // Start enrollment process
+    const res: VerifyResponse = await response.json();
+    import("@privacybydesign/yivi-frontend").then((yivi) => {
+      const issuance = yivi.newPopup({
+        language: i18n.language,
+        session: {
+          url: res.irma_server_url,
+          start: {
+            method: "POST",
+            headers: {
+              "Content-Type": "text/plain",
+            },
+            body: res.jwt,
+          },
+          result: false,
+        },
+      });
+      issuance
+        .start()
+        .then(() => {
+          setMessage(t("phone_add_success"));
+          setPhoneNumber("");
+          navigate(`/${i18n.language}/done`);
+        })
+        .catch((e: string) => {
+          if (e === "Aborted") {
+            setErrorMessage(t("phone_add_cancel"));
+          } else {
+            setErrorMessage(t("phone_add_error"));
+          }
+        });
+    });
+    return;
+  }} catch (error) {
+    navigate(`/${i18n.language}/error`);
+  }
+};
+
+ // user types the verification code
   const enroll = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setErrorMessage(undefined);
@@ -33,64 +92,19 @@ export default function EnrollPage() {
       return;
     }
 
-    const parsedPhoneNumber = parsePhoneNumberFromString(phoneNumber);
+    await VerifyAndStartIssuance(phoneNumber, token);
+  };
 
-    const response = await fetch(
-      '/verify',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          phone: parsedPhoneNumber?.number,
-          token: token
-        })
-      }
-    );
-
-    if (response.ok) {
-      // Start enrollment process
-      const res: VerifyResponse = await response.json();
-      import("@privacybydesign/yivi-frontend").then((yivi) => {
-        const issuance = yivi.newPopup({
-          language: i18n.language,
-          session: {
-            url: res.irma_server_url,
-            start: {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'text/plain',
-              },
-              body: res.jwt,
-            },
-            result: false,
-          },
-        });
-        issuance.start()
-            .then(() => {
-                setMessage(t("phone_add_success"));
-                setPhoneNumber('');
-                navigate(`/${i18n.language}/done`);
-            })
-            .catch((e: string) => {
-                if (e === 'Aborted') {
-                    setErrorMessage(t("phone_add_cancel"));
-                } else {
-                  setErrorMessage(t("phone_add_error"));
-                }
-            });
-      });
+  // if the user has a deeplink in the SMS we verify and start the issuance
+  useEffect(() => {
+    const hash = window.location.hash;
+    const match = hash.match(/^#!verify:(\+\d+):([0-9A-Za-z]{6})$/);
+    if (!match) {
       return;
     }
-    let errorCode = await response.text();
-    errorCode = errorCode.trim().replaceAll("-", "_").replaceAll(":", "_").toLowerCase();
-    if (errorCode) {
-      setErrorMessage(t(errorCode));
-    } else {
-      navigate(`/${i18n.language}/error`);
-    }
-  };
+    const [, linkPhoneNumber, linkToken] = match;
+    VerifyAndStartIssuance(linkPhoneNumber, linkToken);
+  }, [navigate, setPhoneNumber, t]);
 
   return (
     <>
